0b972fc5332cef69e6528f8c2e7c003f6876575c
HalmstadV <pikdokenaral@gmail.com>
2018-04-05 00:02:11 +0300

    first product xD

--- main/Source2.py
+++ main/Source2.py
@@ -10,6 +10,8 @@ removePunctuation = str.maketrans('', '', string.punctuation)
 stopWordList = list()
 dictionary = {}
 myList = ["1","2","3"]
+firstSentenceScores = []
+analysedSentenceScores =[]
 
 
 
@@ -27,6 +29,7 @@ def sentimentAnalyze(filename,**dictionary):
     for i in range(1000):
         positiveWordCount = 0;
         negativeWordCount = 0;
+        sentenceSentimentScore = 0;
         string3 = textfile.readline()
         splitted_line = string3.split()
 
@@ -40,12 +43,24 @@ def sentimentAnalyze(filename,**dictionary):
                 elif word_sentiment.__eq__("negative"):
                     negativeWordCount+=1
 
+        if positiveWordCount==0 and negativeWordCount==0:
+            sentenceSentimentScore=-0
+            analysedSentenceScores.append("0")
+        elif positiveWordCount>negativeWordCount:
+            sentenceSentimentScore=1
+            analysedSentenceScores.append("1")
+        elif negativeWordCount>positiveWordCount:
+            sentenceSentimentScore=0
+            analysedSentenceScores.append("0")
+        elif positiveWordCount==negativeWordCount:
+            sentenceSentimentScore=0
+            analysedSentenceScores.append("0")
 
+        # print( stringLine+ " -  Negative word count : {} , Positive word count {}".format(negativeWordCount,
+        #                                                                                  positiveWordCount))
 
-        print( stringLine+ " -  Negative word count : {} , Positive word count {}".format(negativeWordCount,
-                                                                                         positiveWordCount))
-
-
+        print( stringLine +" Sentence Score : {}  ".format(sentenceSentimentScore))
+    # print(analysedSentenceScores)
 
 
 
@@ -74,8 +89,10 @@ for i in range(stopwordsLineCount):
         datasetline = file1.readline().lower().translate(removePunctuation)
         splitdatasetline = datasetline.split()
         if splitdatasetline[len(splitdatasetline)-1].__contains__("0"):
+            firstSentenceScores.append("0")
             ZeroCount+=1
         elif splitdatasetline[len(splitdatasetline)-1].__contains__("1"):
+            firstSentenceScores.append("1")
             OneCount+=1
         # datasetline = file1.readline().lower().translate(removePunctuation)
 
@@ -93,4 +110,33 @@ print("Number of 0 :{} , Number of 1 : {}".format(ZeroCount,OneCount))
 
 
 sentimentAnalyze("filteredtext.txt",**dictionary)
+
+SuccessCount = 0;
+FailCount = 0;
+
+for i in range(1001):
+
+    num1 = firstSentenceScores[int(i-1)]
+    num2 = analysedSentenceScores[int(i-1)]
+
+    if num1 == num2:
+        print("Successful guess")
+        SuccessCount+=1
+
+    else:
+        print("Failed guess")
+        FailCount+=1
+
+    # if analysedSentenceScores[int(i)] is firstSentenceScores[int(i)]:
+    #     print("Successful guess")
+    #     SuccessCount+=1
+    # elif analysedSentenceScores[int(i)] is not firstSentenceScores[int(i)]:
+    #     print("Failed guess")
+    #     FailCount+=1
+
+
+print("No Successful Guesses : {} ".format(SuccessCount))
+print("No Failed Guesses : {}".format(FailCount))
+
+
 
